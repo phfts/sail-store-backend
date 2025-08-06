9f3a4a5d3389bd106830307af80da466c160d761
Paulo Henrique Soares <phsoares.ita@gmail.com>
2025-08-06 17:39:52 -0300

    Fix AddDescriptionToCompanies migration to check if column exists

--- db/migrate/20250806185648_add_description_to_companies.rb
+++ db/migrate/20250806185648_add_description_to_companies.rb
@@ -1,5 +1,7 @@
 class AddDescriptionToCompanies < ActiveRecord::Migration[8.0]
   def change
-    add_column :companies, :description, :text
+    unless column_exists?(:companies, :description)
+      add_column :companies, :description, :text
+    end
   end
 end
